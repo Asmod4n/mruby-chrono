62cd1269e1726d36067336c4513c5ba01a3944d3
Asmod4n <Asmod4n@users.noreply.github.com>
2016-05-10 21:16:37 +0200

    add System clock, small cleanups

--- src/mrb-chrono.c
+++ src/mrb-chrono.c
@@ -4,14 +4,14 @@
 #if _POSIX_TIMERS > 0 && defined(_POSIX_MONOTONIC_CLOCK)
 #include <time.h>
 
-MRB_INLINE mrb_float
-mrb_chrono_monotonic_now()
+static mrb_value
+mrb_chrono_steady_now(mrb_state *mrb, mrb_value self)
 {
   struct timespec ts;
 
   clock_gettime(CLOCK_MONOTONIC, &ts);
 
-  return (mrb_float) ts.tv_sec + ((mrb_float) ts.tv_nsec / 1000000000.0));
+  return mrb_float_value(mrb, ts.tv_sec + (ts.tv_nsec / 1000000000.0)));
 }
 
 #elif defined(__MACH__)
@@ -23,52 +23,72 @@ static void __attribute__((constructor)) sTimebaseInfo_init()
   mach_timebase_info(&sTimebaseInfo);
 }
 
-MRB_INLINE mrb_float
-mrb_chrono_monotonic_now()
+static mrb_value
+mrb_chrono_steady_now(mrb_state *mrb, mrb_value self)
 {
-  return (mrb_float) mach_absolute_time() * (mrb_float) sTimebaseInfo.numer / (mrb_float) sTimebaseInfo.denom / 1000000000.0;
+  return mrb_float_value(mrb, mach_absolute_time() / 1000000000.0 / sTimebaseInfo.denom * sTimebaseInfo.numer);
 }
 
 #elif defined(_MSC_VER)
 #include <windows.h>
 #include <assert.h>
 
-MRB_INLINE mrb_float
-mrb_chrono_monotonic_now()
+static mrb_value
+mrb_chrono_steady_now(mrb_state *mrb, mrb_value self)
 {
   static mrb_float frequency = 0.0;
 
   if (frequency == 0.0) {
     LARGE_INTEGER freq;
-    QueryPerformanceFrequency (&freq);
+    QueryPerformanceFrequency(&freq);
     assert (freq.QuadPart != 0);
-    frequency = (mrb_float) freq.QuadPart / 1000.0;
+    frequency = freq.QuadPart / 1000.0;
   }
 
   LARGE_INTEGER count;
-  QueryPerformanceCounter (&count);
+  QueryPerformanceCounter(&count);
 
-  return (mrb_float) cound.QuadPart / frequency;
+  return mrb_float_value(mrb, cound.QuadPart / frequency);
 }
 
 #else
-#error "cannot find a monotonic time source"
+#error "cannot find a monotonic time clock"
+#endif
+
+#ifndef _MSC_VER
+#include <sys/time.h>
 #endif
 
 static mrb_value
-mrb_chrono_steady_now(mrb_state *mrb, mrb_value self)
+mrb_chrono_system_now(mrb_state *mrb, mrb_value self)
 {
-  return mrb_float_value(mrb, mrb_chrono_monotonic_now());
+#ifdef _MSC_VER
+  FILETIME ft;
+  GetSystemTimeAsFileTime(&ft);
+
+  ULARGE_INTEGER dateTime;
+  memcpy(&dateTime, ft, sizeof(dateTime));
+
+  return mrb_float_value(mrb, dateTime.QuadPart / 10000.0);
+#else
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+
+  return mrb_float_value(mrb, tv.tv_sec + (tv.tv_usec / 1000000.0));
+#endif
 }
 
 void
 mrb_mruby_chrono_gem_init(mrb_state* mrb)
 {
-  struct RClass *chrono_mod, *steady_mod;
+  struct RClass *chrono_mod, *steady_mod, *system_mod;
   chrono_mod = mrb_define_module(mrb, "Chrono");
-  steady_mod = mrb_define_module_under(mrb, chrono_mod, "Steady");
 
+  steady_mod = mrb_define_module_under(mrb, chrono_mod, "Steady");
   mrb_define_module_function(mrb, steady_mod, "now", mrb_chrono_steady_now, MRB_ARGS_NONE());
+
+  system_mod = mrb_define_module_under(mrb, chrono_mod, "System");
+  mrb_define_module_function(mrb, system_mod, "now", mrb_chrono_system_now, MRB_ARGS_NONE());
 }
 
 void mrb_mruby_chrono_gem_final(mrb_state* mrb) {}
